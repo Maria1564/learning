8a174b24251255e2dce14829e7a8f878fe21c760
maria <salyginamaria156@gmail.com>
2025-01-20 15:35:57 +0300

    добавлен фунционал

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,59 @@
-import { useEffect } from 'react'
-import './App.css'
-import { usePostsStore } from './store/storePosts'
+import { useEffect, useRef } from "react";
+import "./App.css";
+import { usePostsStore } from "./store/storePosts";
 
 function App() {
- const getPosts =  usePostsStore(state=> state.getAllPosts)
- const deletePosts =  usePostsStore(state=> state.deletePost)
- const posts =  usePostsStore(state=> state.posts)
- useEffect(()=>{getPosts()}, [])
+  const inpTitle = useRef<HTMLInputElement | null>(null);
+  const inpDescription = useRef<HTMLTextAreaElement | null>(null);
 
- const deleteSelectedPost = (id: number) => {
-  deletePosts(id)
- }
+  const getPosts = usePostsStore(state => state.getAllPosts);
+  const deletePosts = usePostsStore(state => state.deletePost);
+  const createNewPost = usePostsStore(state => state.createNewPost);
+
+  const posts = usePostsStore(state => state.posts);
+
+  useEffect(() => {
+    getPosts();
+    const unsubscribeState  =usePostsStore.subscribe(({posts}, prevState)=> {
+      console.log("state >> ", posts),
+      console.log("prevState >> ", prevState.posts)
+    })
+
+    return () => unsubscribeState()
+  }, []);
+
+  const deleteSelectedPost = (id: number) => {
+    deletePosts(id);
+    
+  };
+
+  const handlerClick = () => {
+    if (inpTitle.current && inpDescription.current) {
+      createNewPost({
+        id: posts[posts.length - 1].id + 1,
+        title: inpTitle.current.value,
+        description: inpDescription.current.value,
+      });
+    }
+  };
   return (
-    <div className=''> 
-    {
-      posts?.map((item)=> (
-        <div className='card' key={item.id}>
+    <div className="">
+      <div className="new_post">
+        <h2>New post</h2>
+        <input ref={inpTitle} type="text" placeholder="title" />
+        <textarea ref={inpDescription} placeholder="description" />
+        <button onClick={handlerClick}>create</button>
+      </div>
+      {posts?.map(item => (
+        <div className="card" key={item.id}>
           <h2>{item.title}</h2>
           <p>{item.description}</p>
           <button onClick={() => deleteSelectedPost(item.id)}>delete</button>
         </div>
-      ))
-    }
-    <div className="new_post"></div>
+      ))}
+      <div className="new_post"></div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/store/storePosts.ts
+++ src/store/storePosts.ts
@@ -1,34 +1,43 @@
-import axios from "axios"
-import {create} from "zustand"
-import { persist } from "zustand/middleware"
+import axios from "axios";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type Post = {
-    id: number,
-    title: string,
-    description: string
-}
+  id: number;
+  title: string;
+  description: string;
+};
 
-type PostsStoreState = {posts: Post[]}
+type PostsStoreState = { posts: Post[] };
 
 type PostsStoreAction = {
-    getAllPosts: ()=>void,
-    deletePost: (id: number) => void
-}
+  getAllPosts: () => void;
+  deletePost: (id: number) => void;
+  createNewPost: (newPost: Post) => void;
+};
 
-type PostsStore = PostsStoreState & PostsStoreAction
+type PostsStore = PostsStoreState & PostsStoreAction;
 
 export const usePostsStore = create<PostsStore>()(
-    persist(
-        (set)=> ({
-            posts: [],
-            getAllPosts: ()=> {
-                axios.get('https://dummyjson.com/products')
-                .then(({data})=>set({ posts: data.products }))
-            },
-            deletePost: (selectId: number) => {
-                set((state) => ({posts: state.posts.filter((item)=> item.id !== selectId)}))
-            }
-        }),
-        {name: "posts-store"}
-    )
-)
+  persist(
+    (set, get) => ({
+      posts: [],
+
+      getAllPosts: () => {
+        if (get().posts.length) return;
+        axios
+          .get("https://dummyjson.com/products")
+          .then(({ data }) => set({ posts: data.products }));
+      },
+      deletePost: (selectId: number) => {
+        set(state => ({
+          posts: state.posts.filter(item => item.id !== selectId),
+        }));
+      },
+      createNewPost: newPost => {
+        set(state => ({ posts: [...state.posts, newPost ] }));
+      },
+    }),
+    { name: "posts-store" }
+  )
+);
