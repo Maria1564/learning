f5232a03ec699e9650841c8a7417ab49e42f4a6d
maria <salyginamaria156@gmail.com>
2025-02-07 12:11:36 +0300

    private folders

---
+++ app/%5Fprivate/page.tsx
@@ -0,0 +1,5 @@
+const PrivatePage = () => {
+    return <h2>Hello</h2>
+}
+
+export default PrivatePage
---
+++ app/dashboard/line-chart.tsx
@@ -0,0 +1,5 @@
+const LineChart = () => {
+    return <h1>This Line chart</h1>
+}
+
+export default LineChart
