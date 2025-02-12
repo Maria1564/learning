86f1d6e76362755e66504414e69cec9b5eb56253
maria <salyginamaria156@gmail.com>
2025-02-12 16:11:45 +0300

    layout

---
+++ app/products/[productId]/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const BlogLayout = ({children}: {
+    children: React.ReactNode
+  }) => {
+    return (<>
+    
+    {children}
+    <h3>Features products</h3>
+    </>
+
+    )
+}
+
+export default BlogLayout
